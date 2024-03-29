519dea48dbd4498f967018e037b4f3166d7d3fc3
ping1642 <ping1642@gmail.com>
2024-03-29 23:04:24 +0800

    初始化 Spring Boot project

---
+++ src/test/java/com/cping/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cping.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
